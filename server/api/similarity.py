e7164635b6a944280d1df87204a36caba875b462
Gordon Gao <gordon@ivy.com>
2017-01-12 16:31:47 -0500

    refactor api

--- server/api/similarity.py
+++ server/api/similarity.py
@@ -1,20 +1,25 @@
 from flask import Blueprint, json, jsonify
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from services.similarity_service import *
 from services.mongodb import *
 
 similarity_api = Api(Blueprint('similarity_api', __name__))
 
-@similarity_api.resource('/sim/e/<int:id>')
-class EventSimilarityAPI(Resource):
-    @staticmethod
-    def get(id):
-    	return events_sim(id).to_json(orient='records')
+# @similarity_api.resource('/sim/e/<int:id>')
+# class EventSimilarityAPI(Resource):
+#     @staticmethod
+#     def get(id):
+#     	return events_sim(id).to_json(orient='records')
 
-@similarity_api.resource('/sim/e/<location>/<int:id>')
+@similarity_api.resource('/sim/e/<int:id>')
 class EventLocationSimilarityAPI(Resource):
     @staticmethod
-    def get(id, location):
+    def get(id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('location', type=str, help='Yada Yada Yada')
+        args = parser.parse_args()
+        location = args['location']
+
     	events_sim = events_sim_with_loc(id, location)
         update_events_table(id, events_sim)
         return events_sim.to_json(orient='records')
