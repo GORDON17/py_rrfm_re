b3b289ef675fd91fd6abacc6ab65493b528e0d42
Gordon Gao <gordon@ivy.com>
2017-01-25 10:09:59 -0500

    set jobs in url

--- server/api/job_api.py
+++ server/api/job_api.py
@@ -1,10 +1,15 @@
 from apscheduler.schedulers import SchedulerNotRunningError
 from flask import Blueprint, json, jsonify
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 from services.job_service import *
 from services.mongodb import get_jobs
 from configurations.env_configs import *
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+s = BackgroundScheduler()
+# sched = Scheduler(s)
 
 job_api = Api(Blueprint('job_api', __name__))
 
@@ -12,11 +17,46 @@ job_api = Api(Blueprint('job_api', __name__))
 class JobStartAPI(Resource):
     @staticmethod
     def get():
+			parser = reqparse.RequestParser()
+			parser.add_argument('day', type=int, help='Yada Yada Yada')
+			parser.add_argument('hour', type=int, help='Yada Yada Yada')
+			parser.add_argument('minute', type=int, help='Yada Yada Yada')
+			args = parser.parse_args()
+			day = args['day']
+			hour = args['hour']
+			minute = args['minute']
+			print(day, hour, minute)
 			try:
-				sched.start()
+				# global sched
+				global s
+				s.add_job(interest_similarity_job, 'cron', 
+												day_of_week=day, 
+												hour=hour, 
+												minute=minute,
+												id='0',
+												name='social_interest_similarity')
+				s.add_job(mutual_friends_job, 'cron', 
+												day_of_week=day, 
+												hour=hour, 
+												minute=minute,
+												id='1',
+												name='mutual_friend')
+				s.start()
+				# sched.schedule_jobs(day_of_week=2, hour=9, minute=33)
+				# sched.start()
 				return {'status': 200, 'message': 'The scheduler is running.'}
 			except:
 				return {'status': 400, 'message': 'The scheduler is failed to start.'}
+
+@job_api.resource("/printjobs")
+class PrintJobsAPI(Resource):
+    @staticmethod
+    def get():
+			try:
+				sched.print_jobs()
+				return {'status': 200, 'message': 'Printed'}
+			except:
+				return {'status': 400, 'message': 'Cannot print jobs'}
 
 
 @job_api.resource("/stopsched")
@@ -37,9 +77,6 @@ class JobListAPI(Resource):
 				return get_jobs()
 			except:
 				return {'status': 400, 'message': 'Could not retrieve job list.'}
-
-
-
 
 
 
--- server/services/job_service.py
+++ server/services/job_service.py
@@ -10,48 +10,102 @@ from services.bfs_service import process_mutual_friends
 from configurations.env_configs import *
 from configurations.development import *
 
-from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.jobstores.mongodb import MongoDBJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
 
 from multiprocessing import Process
 
-# def sched_listener(event):
-# 	print event
 
-# executors = {
-#     'default': ProcessPoolExecutor(4)
-# }
+class Scheduler:
+	def __init__(self, sched):
+		self.sched = sched
 
-sched = BackgroundScheduler()#executors=executors)
-# sched.add_listener(sched_listener)
-day_of_week = 1
-job_hour = 15
-job_minute = 38
+	def schedule_jobs(self, day_of_week, hour, minute):
+		self.sched.add_job(self.interest_similarity_job, 'cron', 
+												day_of_week=day_of_week, 
+												hour=hour, 
+												minute=minute,
+												id='0',
+												name='social_interest_similarity')
+		self.sched.add_job(self.mutual_friends_job, 'cron', 
+												day_of_week=day_of_week, 
+												hour=hour, 
+												minute=minute,
+												id='1',
+												name='mutual_friend')
+		# self.sched.start()
 
-@sched.scheduled_job('cron', day_of_week=day_of_week, hour=job_hour, minute=job_minute, id='0', name='social_interest_similarity')
-def scheduled_job():
-		# job_id = '0'
-		# job_name = 'social_interest_similarity'
-		# print('social_interest_similarity job is pendding.')
+	def start(self):
+		self.sched.start()
+		self.sched.print_jobs()
 
-		# job_obj_id = add_job(job_id=job_id, name=job_name)
-		
-		# print('social_interest_similarity job is running.')
-		# p = Process(target=process_interest_similarity, args=(SOCIAL_INTERESTS_URI, INTEREST_TYPES['social']))
-		# p.start()
-		# # process_interest_similarity(SOCIAL_INTERESTS_URI, INTEREST_TYPES['social'])
+	def print_jobs(self):
+		self.sched.print_jobs()	
 
-		# if job_obj_id:
-		# 	update_job_state(job_obj_id=job_obj_id, state=1)
-		# 	print('social_interest_similarity job is finished.')
-		# else:
-		# 	print('there is no such job')
+	def shutdown(self, wait=False):
+		self.sched.shutdown(wait=wait)
+		self.sched.remove_all_jobs()
+
+	def interest_similarity_job(self):
 		p = Process(target=mp_process_interest_similarity, args=())
 		p.start()
 
+	def mp_process_interest_similarity(self):
+		connect_db()
+		job_id = '0'
+		job_name = 'social_interest_similarity'
+		print('social_interest_similarity job is pendding.')
+
+		job_obj_id = add_job(job_id=job_id, name=job_name)
+		
+		print('social_interest_similarity job is running.')
+		process_interest_similarity(SOCIAL_INTERESTS_URI, INTEREST_TYPES['social'])
+
+		if job_obj_id:
+			update_job_state(job_obj_id=job_obj_id, state=1)
+			print('social_interest_similarity job is finished.')
+		else:
+			print('there is no such job')
+
+
+	def mutual_friends_job(self):
+		p = Process(target=mp_process_mutual_friends, args=())
+		p.start()
+
+	def mp_process_mutual_friends():
+			connect_db()
+			job_id = '1'
+			job_name = 'mutual_friend'
+			print('mutual_friend job is pendding.')
+
+			job_obj_id = add_job(job_id=job_id, name=job_name)
+			
+			print('mutual_friend job is running.')
+			process_mutual_friends(CONNECTIONS_URI)
+
+			if job_obj_id:
+				update_job_state(job_obj_id=job_obj_id, state=1)
+				print('mutual_friend job is finished.')
+			else:
+				print('there is no such job')
+
+
+
+
+# sched = BackgroundScheduler()
+# day_of_week = 1
+# job_hour = 15
+# job_minute = 38
+
+# @sched.scheduled_job('cron', day_of_week=day_of_week, hour=job_hour, minute=job_minute, id='0', name='social_interest_similarity')
+# def scheduled_job():
+# 		p = Process(target=mp_process_interest_similarity, args=())
+# 		p.start()
+
+def interest_similarity_job():
+		p = Process(target=mp_process_interest_similarity, args=())
+		p.start()
 
 def mp_process_interest_similarity():
 		connect_db()
@@ -70,26 +124,12 @@ def mp_process_interest_similarity():
 		else:
 			print('there is no such job')
 
+# @sched.scheduled_job('cron', day_of_week=day_of_week, hour=job_hour, minute=job_minute, id='1', name='mutual_friend')
+# def scheduled_job():
+# 		p = Process(target=mp_process_mutual_friends, args=())
+# 		p.start()
 
-
-@sched.scheduled_job('cron', day_of_week=day_of_week, hour=job_hour, minute=job_minute, id='1', name='mutual_friend')
-def scheduled_job():
-		# job_id = '1'
-		# job_name = 'mutual_friend'
-		# print('mutual_friend job is pendding.')
-
-		# job_obj_id = add_job(job_id=job_id, name=job_name)
-		
-		# print('mutual_friend job is running.')
-		# p = Process(target=process_mutual_friends, args=(CONNECTIONS_URI,))
-		# p.start()
-		# # process_mutual_friends(CONNECTIONS_URI)
-
-		# if job_obj_id:
-		# 	update_job_state(job_obj_id=job_obj_id, state=1)
-		# 	print('mutual_friend job is finished.')
-		# else:
-		# 	print('there is no such job')
+def mutual_friends_job():
 		p = Process(target=mp_process_mutual_friends, args=())
 		p.start()
 
