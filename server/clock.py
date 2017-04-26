4cbf9085cee0db01896097659f6d28669195a7e8
Gordon Gao <gordon@ivy.com>
2017-04-26 16:41:23 -0400

    set jobs time

--- server/clock.py
+++ server/clock.py
@@ -20,35 +20,35 @@ def social_interest_similarity_job_with_lcn():
 		print "social_interest_similarity_job_with_lcn is failed!"
 
 
-# @scheduler.scheduled_job('cron', day_of_week=os.environ.get('G_DAY'), hour=os.environ.get('G_HOUR'), minute=os.environ.get('G_MINUTE'), id='02', name=JOB['GENERATE']['SOCIAL_INTEREST_SIMILARITY'])
-# def mutual_friend_job_with_lcn():
-# 	params = {
-# 		'location': True,
-# 		'chapter': True,
-# 		'nationality': True
-# 	}
-# 	try:
-# 		print "mutual_friend_job_with_lcn is running."
-# 		g_mutual_friends_job(params)
-# 	except:
-# 		print "mutual_friend_job_with_lcn is failed!"
+@scheduler.scheduled_job('cron', day_of_week=os.environ.get('G_DAY'), hour=os.environ.get('G_HOUR')+6, minute=os.environ.get('G_MINUTE'), id='02', name=JOB['GENERATE']['SOCIAL_INTEREST_SIMILARITY'])
+def mutual_friend_job_with_lcn():
+	params = {
+		'location': True,
+		'chapter': True,
+		'nationality': True
+	}
+	try:
+		print "mutual_friend_job_with_lcn is running."
+		g_mutual_friends_job(params)
+	except:
+		print "mutual_friend_job_with_lcn is failed!"
 
 
-# @scheduler.scheduled_job('cron', day_of_week=os.environ.get('R_DAY'), hour=os.environ.get('R_HOUR'), minute=os.environ.get('R_MINUTE'), id='11', name=JOB['RETRIEVE']['SOCIAL_INTEREST_SIMILARITY'])
-# def interest_similarity_sqs_job():
-# 	try:
-# 		print "interest_similarity_sqs_job is running."
-# 		r_interest_similarity_job()
-# 	except:
-# 		print "interest_similarity_sqs_job is failed!"
+@scheduler.scheduled_job('cron', day_of_week=os.environ.get('R_DAY'), hour=os.environ.get('R_HOUR'), minute=os.environ.get('R_MINUTE'), id='11', name=JOB['RETRIEVE']['SOCIAL_INTEREST_SIMILARITY'])
+def interest_similarity_sqs_job():
+	try:
+		print "interest_similarity_sqs_job is running."
+		r_interest_similarity_job()
+	except:
+		print "interest_similarity_sqs_job is failed!"
 
-# @scheduler.scheduled_job('cron', day_of_week=os.environ.get('R_DAY'), hour=os.environ.get('R_HOUR'), minute=os.environ.get('R_MINUTE'), id='12', name=JOB['RETRIEVE']['SOCIAL_INTEREST_SIMILARITY'])
-# def mutual_friend_sqs_job():
-# 	try:
-# 		print "mutual_friend_sqs_job is running."
-# 		r_mutual_friend_job()
-# 	except:
-# 		print "mutual_friend_sqs_job is failed!"
+@scheduler.scheduled_job('cron', day_of_week=os.environ.get('R_DAY'), hour=os.environ.get('R_HOUR'), minute=os.environ.get('R_MINUTE'), id='12', name=JOB['RETRIEVE']['SOCIAL_INTEREST_SIMILARITY'])
+def mutual_friend_sqs_job():
+	try:
+		print "mutual_friend_sqs_job is running."
+		r_mutual_friend_job()
+	except:
+		print "mutual_friend_sqs_job is failed!"
 
 
 
