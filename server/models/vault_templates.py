ef0ca7d4e95dc0a855c8d084e2a98d84c15db008
Gordon Gao <gordon@ivy.com>
2017-05-10 18:28:52 -0400

    change templates

--- server/models/vault_templates.py
+++ server/models/vault_templates.py
@@ -2,18 +2,18 @@ def interest_template(isPlain, user_id, sim_percent):
 	percent = int(sim_percent * 100)
 
 	if isPlain:
-		text = "Talk to {{user-%d}} as you have %d%% interests in common" \
+		text = "Meet {{user-%d}} as you have %d%% interests in common" \
 					% (user_id, percent)
 	else:
-		text = "Talk to <b>{{user-%d}}</b> as you have %d%% interests in common" \
+		text = "Meet <b>{{user-%d}}</b> as you have %d%% interests in common" \
 					% (user_id, percent)
 
 	return text
 
 def mutual_template(isPlain, user_id, num_of_mutual_friends):
 	if isPlain:
-		text = "Talk to {{user-%d}} as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
+		text = "Meet {{user-%d}} as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
 	else:
-		text = "Talk to <b>{{user-%d}}</b> as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
+		text = "Meet <b>{{user-%d}}</b> as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
 	
 	return text
