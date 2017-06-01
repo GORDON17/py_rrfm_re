b10b4131f300447c5b69b97fe31516b87a88d0b4
Gordon Gao <gordon@ivy.com>
2017-06-01 11:41:32 -0400

    remove pendding connections filter from mutual

--- server/models/vault_templates.py
+++ server/models/vault_templates.py
@@ -2,18 +2,18 @@ def interest_template(isPlain, user_id, sim_percent):
 	percent = int(sim_percent * 100)
 
 	if isPlain:
-		text = "Meet {{user-%d}} as you have %d%% interests in common" \
+		text = "Connect with {{user-%d}} as you have %d%% interests in common" \
 					% (user_id, percent)
 	else:
-		text = "<orange>Meet {{user-%d}}</orange> as you have %d%% interests in common" \
+		text = "<orange>Connect with {{user-%d}}</orange> as you have %d%% interests in common" \
 					% (user_id, percent)
 
 	return text
 
 def mutual_template(isPlain, user_id, num_of_mutual_friends):
 	if isPlain:
-		text = "Meet {{user-%d}} as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
+		text = "Connect with {{user-%d}} as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
 	else:
-		text = "<orange>Meet {{user-%d}}</orange> as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
+		text = "<orange>Connect with {{user-%d}}</orange> as you have %d mutual friends in common" % (user_id, num_of_mutual_friends)
 	
 	return text
