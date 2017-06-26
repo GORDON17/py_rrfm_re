fae73e0a0dd3260eff5436de11fe6322af21081f
Gordon Gao <gordon@ivy.com>
2017-06-26 17:17:22 -0400

    fix worker

--- server/worker.py
+++ server/worker.py
@@ -15,11 +15,13 @@ params = {
 hour=os.environ.get('G_HOUR')
 minute=os.environ.get('G_MINUTE')
 
-scheduler.add_job(g_all_interest_similarity_job, 'cron', args=[params]
-                                                hour=hour, 
-                                                minute=minute,
-                                                id='0',
-                                                name=JOB['GENERATE']['ALL_INTEREST_SIMILARITY'])
+scheduler.add_job(g_all_interest_similarity_job, 
+                    'cron', 
+                    args=[params],
+                    hour=hour, 
+                    minute=minute,
+                    id='0',
+                    name=JOB['GENERATE']['ALL_INTEREST_SIMILARITY'])
 
 
 
